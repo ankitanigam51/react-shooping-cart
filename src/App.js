58fc7acf85333b5948f8b44237b4c4edc8dc027c
Ankita Nigam <ankita.nigam@leptonmaps.com>
2021-12-20 22:30:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="grid-container">
+    //   <header className="header">
+    //    <a href="/"> React Shopping Cart </a> 
+    //     </header>
+    //   <main className="main">
+    //     Product List
+    //   </main>
+    //   <footer className="footer">All right is reserved!</footer>
+    // </div>
+    <div className="container">
+      <header className="header">
+        <a href="/">React Shopping Cart</a>
+        </header>
+      <main className="main">Product List</main>
+      <footer className="footer">All right is reserved!</footer>
     </div>
   );
 }
